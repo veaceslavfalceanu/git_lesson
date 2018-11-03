2e2cd88ed4ba4b6838c494ef8b49905d35e96594
Veaceslav Falceanu <veaceslav.falceanu@gmail.com>
2018-11-03 07:52:48 -0400

    Made some modifications

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
-def my_square(x):
+def my_square(z):
 	"""THis funciton takes a number and multiplieds it
 	Another line of comment
 	"""
-	return(x ** 2);
+	return(z ** 2);
 
-print(my_square(6))
+print(my_square(65))
