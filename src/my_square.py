6cc463cb22e8212cbc7d915df57de594e45903db
veaceslav <vfalceanu@janeiredale.com>
2018-11-03 08:59:22 -0400

    Added more documentaion

--- src/my_square.py
+++ src/my_square.py
@@ -2,10 +2,11 @@ def my_square(y):
 	"""THis funciton takes a number and multiplieds it
 	Another line of comment
 	"""
-	return(y ** 2);
+	return(y *** 2);
 
 def my_square2(x):
 	"""This is a comment
+	this is still commit but different then one before
 	"""
 	return(x*x);
 
