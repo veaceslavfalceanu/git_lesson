8aefbb165e0708ad3bacbe15d27f993efdf70992
veaceslav <vfalceanu@janeiredale.com>
2018-11-03 07:37:23 -0400

    Add new value for the function

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,6 @@
 def my_square(x):
-	***THis funciton takes a number and multiplieds it
-	***
+	"""THis funciton takes a number and multiplieds it
+	"""
 	return(x ** 2);
 
-print(my_square(3))
+print(my_square(4))
